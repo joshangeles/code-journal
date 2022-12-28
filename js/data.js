b4415ce6720c53e0c4c063e1d02a399c2db52d5c
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-28 04:37:53 +0000

    'data' object now saved to localStorage and serializes/deserializes previously saved data correctly. #1

--- js/data.js
+++ js/data.js
@@ -1,15 +1,16 @@
 /* exported data */
-
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
-
 function storageHandler(event) {
-  var dataJSON = JSON.stringify(data.entries);
-  window.localStorage.setItem('entryData', dataJSON);
+  var dataJSON = JSON.stringify(data);
+  window.localStorage.setItem('data', dataJSON);
 }
-
 window.addEventListener('beforeunload', storageHandler);
+var savedData = window.localStorage.getItem('data');
+if (savedData !== null) {
+  data = JSON.parse(savedData);
+}
