0335c64bc2efe3f28abb0a8bc3f5f863eca336a0
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-28 03:31:23 +0000

    Input values in form now store inside new 'entry' object #1

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
 var $previewImage = document.querySelector('img#preview');
-var $photoURLInput = document.querySelector('[name="photo-url"]');
+var $photoURLInput = document.querySelector('[name="photo"]');
 var $entryForm = document.querySelector('form');
 function previewImageHandler(event) {
   $previewImage.setAttribute('src', $photoURLInput.value);
 }
 function submitHandler(event) {
   event.preventDefault();
+  var entry = {};
+  entry.title = $entryForm.elements.title.value;
+  entry.photo = $entryForm.elements.photo.value;
+  entry.notes = $entryForm.elements.notes.value;
 }
 $photoURLInput.addEventListener('input', previewImageHandler);
 $entryForm.addEventListener('submit', submitHandler);
