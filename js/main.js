c94a8ca2ca4f19ae7013a58d2043592fb573d26c
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-28 18:58:41 +0000

    Created 'renderEntry' function to generate DOM Tree for a single entry. #2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// issue-1-create-an-entry: allows users to create entries
 var $previewImage = document.querySelector('img#preview');
 var $photoURLInput = document.querySelector('[name="photo"]');
 var $entryForm = document.querySelector('form');
@@ -18,3 +19,30 @@ function submitHandler(event) {
 }
 $photoURLInput.addEventListener('input', previewImageHandler);
 $entryForm.addEventListener('submit', submitHandler);
+
+// issue-2-can-view-entries: users can view their entries
+function renderEntry(entry) {
+  var $entryListItem = document.createElement('li');
+  var $entryRow = document.createElement('div');
+  var $entryPhotoContainer = document.createElement('div');
+  var $entryPhoto = document.createElement('img');
+  var $entryTextContainer = document.createElement('div');
+  var $entryTitle = document.createElement('h2');
+  var $entryNotes = document.createElement('p');
+  $entryRow.setAttribute('class', 'row');
+  $entryPhotoContainer.setAttribute('class', 'column-half');
+  $entryPhoto.setAttribute('class', 'border-round');
+  $entryPhoto.setAttribute('src', data.entries[entry].photo);
+  $entryTextContainer.setAttribute('class', 'column-half');
+  $entryTitle.textContent = data.entries[entry].title;
+  $entryNotes.textContent = data.entries[entry].notes;
+  $entryListItem.appendChild($entryRow);
+  $entryRow.appendChild($entryPhotoContainer);
+  $entryPhotoContainer.appendChild($entryPhoto);
+  $entryRow.appendChild($entryTextContainer);
+  $entryTextContainer.appendChild($entryTitle);
+  $entryTextContainer.appendChild($entryNotes);
+  return $entryListItem;
+}
+
+renderEntry(0);
