a2871632de3452da97f82accbf3342ff6c859394
Joshua Angeles <mintoesverm@yahoo.com>
2022-12-28 03:13:42 +0000

    Added functionality to prevent page from reloading on form submission. #1

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
-var previewImage = document.querySelector('img#preview');
-var photoURLInput = document.querySelector('[name="photo-url"]');
+var $previewImage = document.querySelector('img#preview');
+var $photoURLInput = document.querySelector('[name="photo-url"]');
+var $entryForm = document.querySelector('form');
 function previewImageHandler(event) {
-  previewImage.setAttribute('src', photoURLInput.value);
+  $previewImage.setAttribute('src', $photoURLInput.value);
 }
-photoURLInput.addEventListener('input', previewImageHandler);
+function submitHandler(event) {
+  event.preventDefault();
+}
+$photoURLInput.addEventListener('input', previewImageHandler);
+$entryForm.addEventListener('submit', submitHandler);
